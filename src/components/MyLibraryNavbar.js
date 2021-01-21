68b6adf8e4c337d27abe9a854043db58210dc633
hodayale <hodayale@gmail.com>
2021-01-21 10:07:14 +0200

    styling navbar

--- src/components/MyLibraryNavbar.js
+++ src/components/MyLibraryNavbar.js
@@ -1,25 +1,36 @@
 import '../icons/love-book.svg';
-import { Form, FormControl, Nav, Navbar, Button } from "react-bootstrap";
-//import { IoIosBook } from 'react-icons/io';
+import { Form, FormControl, Nav, Navbar, Button, Col, InputGroup } from "react-bootstrap";
+import { FcSearch } from 'react-icons/fc';
 
 const MyLibraryNavbar = (props) => {
     const {logo} = props;
     // handleLogin = () => {}
     
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" variant="light" expand="lg">
             <Nav>  
                 <Nav.Link /*onClick={handleLogin}*/>כניסה</Nav.Link>
                 <Nav.Link href="#/signup">הרשמה</Nav.Link>
                 {/* <Nav.Link onClick={handleLogout}>יציאה</Nav.Link> */}
             </Nav>
-            <Form inline>
+            
+            {/* <Form inline>
                 <Button variant="outline-success">חפש</Button>
                 <FormControl type="text" placeholder="חיפוש ספר" className="ml-sm-2 text-right" />
-            </Form>
+            </Form> */}
             
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav"> 
+                <Form className="ml-auto">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>
+                                <FcSearch/>
+                            </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control className="text-right" type="text" placeholder="חפש ספר"/>
+                    </InputGroup>
+                </Form>
                 <Nav className="ml-auto">  
                     <Nav.Link href="#/books">הספרים</Nav.Link>
                 </Nav>
