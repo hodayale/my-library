cdfa0e022743db6a443d145a566e0aee1bf00a89
hodayale <hodayale@gmail.com>
2021-01-24 17:44:00 +0200

    fix checkboxes in BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,75 +1,97 @@
 import { Card, Form } from "react-bootstrap";
 import React from 'react';
 import { Redirect } from "react-router-dom";
-
 import { useEffect } from 'react';
 
-const BookCard = (props) => {
-    const {activeUser, filter, bookId, bookName, bookAuther, bookPicture} = props;
-    //const [BookPicture, setBookPicture] = React.useState(undefined);
-    const [redirect, setRedirect] = React.useState(false);
-    const [bookOwned, setbookOwned] = React.useState(false);
-    const [bookRead, setbookRead] = React.useState(false);
-    const [bookWantToRead, setbookWantToRead] = React.useState(false);
-    const [bookWantToOwn, setbookWantToOwn] = React.useState(false);
-    const [bookLoaned, setbookLoaned] = React.useState('');
+class BookCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect: false,
+            bookOwned: false,
+            bookRead: false,
+            bookWantToRead: false,
+            bookWantToOwn: false,
+            bookLoaned: false
+        }
+    }
+ 
+    
+    // //const [BookPicture, setBookPicture] = React.useState(undefined);
+    // const [redirect, setRedirect] = React.useState(false);
+    // const [bookOwned, setbookOwned] = React.useState(false);
+    // const [bookRead, setbookRead] = React.useState(false);
+    // const [bookWantToRead, setbookWantToRead] = React.useState(false);
+    // const [bookWantToOwn, setbookWantToOwn] = React.useState(false);
+    // const [bookLoaned, setbookLoaned] = React.useState('');
 
-    useEffect(() => {
-        if(activeUser) {
-            const booksOwned = activeUser.booksOwned.split(',');
-            const booksRead = activeUser.booksRead.split(',');
-            const booksWantToRead = activeUser.booksWantToRead.split(',');
-            const booksWantToOwn = activeUser.booksWantToOwn.split(',');
+    componentDidMount() {
+        if(this.props.activeUser) {
+            const booksOwned = this.props.activeUser.booksOwned.split(',');
+            const booksRead = this.props.activeUser.booksRead.split(',');
+            const booksWantToRead = this.props.activeUser.booksWantToRead.split(',');
+            const booksWantToOwn = this.props.activeUser.booksWantToOwn.split(',');
     
-            if(booksOwned.includes(bookId.toString())) {
-                setbookOwned(true);
+            if(booksOwned.includes(this.props.bookId.toString())) {
+                this.setState({bookOwned: true});
             }
     
-            if(booksRead.includes(bookId.toString())) {
-                setbookRead(true);
+            if(booksRead.includes(this.props.bookId.toString())) {
+                this.setState({bookRead: true});
             }
     
-            if(booksWantToRead.includes(bookId.toString())) {
-                setbookWantToRead(true);
+            if(booksWantToRead.includes(this.props.bookId.toString())) {
+                this.setState({bookWantToRead: true});
             }
     
-            if(booksWantToOwn.includes(bookId.toString())) {
-                setbookWantToOwn(true);
+            if(booksWantToOwn.includes(this.props.bookId.toString())) {
+                this.setState({bookWantToOwn: true});
             }        
         }
-    });
-
-    const navigateToBook = () => {
-        setRedirect(true);
-    }
-
-    if(redirect) {
-        return (<Redirect push to={`/books/${bookId}`}/>)
     }
 
     
 
-    const checkboxs =  <Form>
-            <Form.Check type='checkbox' id='books-owned' checked={bookOwned} onChange={(e) => setbookOwned(e.target.value)} label='ברשותי'/>
-            <Form.Check type='checkbox' id='books-read' checked={bookRead} onChange={(e) => setbookRead(e.target.value)} label='קראתי'/>
-            <Form.Check type='checkbox' id='books-want-to-read' checked={bookWantToRead} onChange={(e) => setbookWantToRead(e.target.value)} disabled={bookRead} label='ברצוני לקרוא'/>
-            <Form.Check type='checkbox' id='books-want-to-own' checked={bookWantToOwn} onChange={(e) => setbookWantToOwn(e.target.value)} disabled={bookOwned} label='ברצוני לרכוש'/>
-            {/* <Form.Check type='checkbox' id='books-loaned' checked={bookLoaned} label='ספרים שהשאלתי'/> */}
-        </Form>;
+    navigateToBook = () => {
+        this.setState({redirect: true});
+    };
+
+    render() {
+        if(this.state.redirect) {
+            return (<Redirect push to={`/books/${this.props.bookId}`}/>)
+        }
+
+    
+        const checkboxs =  <Form>
+                {(this.props.active != 'owned') ? <Form.Check type='checkbox' id='books-owned' checked={this.state.bookOwned} onChange={() => this.setState({bookOwned: !this.state.bookOwned, 
+                                                                                                                                                         bookWantToOwn: (this.state.bookOwned) ? false : this.setState.bookWantToOwn})} 
+                                value={this.state.bookOwned} label='ברשותי'/> : ""}
+                {(this.props.active != 'read') ? <Form.Check type='checkbox' id='books-read' checked={this.state.bookRead} onChange={() => this.setState({bookRead: !this.state.bookRead, 
+                                                                                                                                                         bookWantToRead: (this.state.bookRead) ? false : this.setState.bookWantToRead })} 
+                                value={this.state.bookRead} label='קראתי'/> : ""}
+                {(this.props.active != 'wantToRead') ? <Form.Check type='checkbox' id='books-want-to-read' checked={this.state.bookWantToRead} 
+                                onChange={() => this.setState({bookWantToRead: !this.state.bookWantToRead})} 
+                                value={this.state.bookWantToRead} disabled={this.state.bookRead} label='ברצוני לקרוא'/> : ""}
+                {(this.props.active != 'wantToOwn') ? <Form.Check type='checkbox' id='books-want-to-own' checked={this.state.bookWantToOwn} 
+                                onChange={() => this.setState({bookWantToOwn: !this.state.bookWantToOwn})} 
+                                value={this.state.bookWantToOwn} disabled={this.state.bookOwned} label='ברצוני לרכוש'/> : ""}
+                {/* <Form.Check type='checkbox' id='books-loaned' checked={bookLoaned} label='ספרים שהשאלתי'/> */}
+            </Form>;
 
     
 
-    return(
-        // <Card style={{ width: '18rem', display: bookName.toLowerCase().includes(filter.toLowerCase()) ? "" : "none"}} onDoubleClick={navigateToBook}> 
-        <Card className="card-design bg-light" style={{ width: '18rem', marginBottom: '20px', display: bookName.includes(filter) ? "" : "none"}} onDoubleClick={navigateToBook}>  
-            <Card.Body>
-                <Card.Img variant="top" src={bookPicture} />
-                <Card.Title className="text-center">{bookName}</Card.Title>
-                <Card.Text className="text-center">{bookAuther}</Card.Text>
-                {activeUser ? checkboxs : ''}
-            </Card.Body>
-        </Card>
-    );
+        return(
+            // <Card style={{ width: '18rem', display: bookName.toLowerCase().includes(filter.toLowerCase()) ? "" : "none"}} onDoubleClick={navigateToBook}> 
+            <Card className="card-design bg-light" style={{ width: '18rem', marginBottom: '20px', display: this.props.bookName.includes(this.props.filter) ? "" : "none"}} onDoubleClick={this.navigateToBook}>  
+                <Card.Body>
+                    <Card.Img variant="top" src={this.props.bookPicture} />
+                    <Card.Title className="text-center">{this.props.bookName}</Card.Title>
+                    <Card.Text className="text-center">{this.props.bookAuther}</Card.Text>
+                    {this.props.activeUser ? checkboxs : ''}
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 
 export default BookCard;
--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,48 +1,56 @@
-import { Col, ListGroup, Row, Tab } from "react-bootstrap";
+import { Col, ListGroup, Row } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 import React from 'react';
 import BookCard from "../components/BookCard";
+//import { useEffect } from 'react';
 
 const BooksPage = (props) => {
     const {activeUser, books, filter} = props;
     const [booksToShow, setbooksToShow] = React.useState('');
+    const [active, setactive] = React.useState('owned');
     
+    // useEffect(() => {
+    //     handleBooks(activeUser.booksOwned);
+    // }, [booksToShow]);
 
     if(!activeUser) {
         return <Redirect push to="/"/>;
     }
 
-    const handleBooks = (booksIds) => {
+    const handleBooks = (booksIds, activeStr) => {
         const booksIdArr = booksIds.split(','); 
         
         setbooksToShow(booksIdArr.map((id) => {
             const book = books.filter(book => book.id == id);
             if (book.length > 0) {
-                return <BookCard key={book.id} activeUser={activeUser} filter={filter} bookId={book[0].id} bookName={book[0].bookName} bookAuther={book[0].auther} bookPicture={book[0].bookCover}/>
+                return <BookCard key={book.id} activeUser={activeUser} filter={filter} bookId={book[0].id} bookName={book[0].bookName} bookAuther={book[0].auther} bookPicture={book[0].bookCover} active={activeStr}/>
             }
             return "";
         }));
     } 
 
     return(
-       <Tab.Container defaultActiveKey="#link1">
-            <Row>
-                <Col sm={2}>
-                    <ListGroup>
-                        <ListGroup.Item action onClick={() => {handleBooks(activeUser.booksOwned)}} variant="danger">ספרים שברשותי</ListGroup.Item>
-                        <ListGroup.Item action onClick={() => {handleBooks(activeUser.booksRead)}} variant="danger">ספרים שקראתי</ListGroup.Item>
-                        <ListGroup.Item action onClick={() => {handleBooks(activeUser.booksWantToRead)}} variant="danger">ספרים שברצוני לקרוא</ListGroup.Item>
-                        <ListGroup.Item action onClick={() => {handleBooks(activeUser.booksWantToOwn)}} variant="danger">ספרים שברצוני לרכוש</ListGroup.Item>
-                        <ListGroup.Item action onClick={() => {handleBooks(activeUser.booksLoaned)}} variant="danger">ספרים שהשאלתי</ListGroup.Item>
-                    </ListGroup>
-                 </Col>
-                <Col sm={10}>
-                    <Row className="justify-content-even">
-                        {booksToShow}
-                    </Row>
+        <Row>
+            <Col sm={2}>
+                <ListGroup>
+                    <ListGroup.Item className={active == 'owned' ? "active" : ""}
+                                    action onClick={() => {setactive('owned'); handleBooks(activeUser.booksOwned, 'owned');}} variant="danger">ספרים שברשותי</ListGroup.Item>
+                    <ListGroup.Item className={active == 'read' ? "active" : ""}
+                                    action onClick={() => {setactive('read'); handleBooks(activeUser.booksRead, 'read')}} variant="danger">ספרים שקראתי</ListGroup.Item>
+                    <ListGroup.Item className={active == 'wantToRead' ? "active" : ""}
+                                    action onClick={() => {setactive('wantToRead'); handleBooks(activeUser.booksWantToRead,'wantToRead')}} variant="danger">ספרים שברצוני לקרוא</ListGroup.Item>
+                    <ListGroup.Item className={active == 'wantToOwned' ? "active" : ""}
+                                    action onClick={() => {setactive('wantToOwned'); handleBooks(activeUser.booksWantToOwn,'wantToOwned')}} variant="danger">ספרים שברצוני לרכוש</ListGroup.Item>
+                    <ListGroup.Item className={active == 'loaned' ? "active" : ""}
+                                    action onClick={() => {setactive('loaned'); handleBooks(activeUser.booksLoaned)}} variant="danger">ספרים שהשאלתי</ListGroup.Item>
+                </ListGroup>
                 </Col>
-            </Row>
-        </Tab.Container>
+            <Col sm={10}>
+                <Row className="justify-content-even">
+                    {booksToShow}
+                </Row>
+            </Col>
+        </Row>
     );
 }
 
